810253b833233c88c7db38ceed8dca53eb6cb84c
AndreMorad <56093049+AndreMorad@users.noreply.github.com>
2020-04-29 15:28:22 +0200

    @andremorad
    Add folders: Controllers, Models, Services and ConfigureServices.
    
    Add packages for Environment:
    
    Microsoft.EntityFramework.Core
    Microsoft.EntityFramework.SqlServer
    Microsoft.EntityFramework.Tools
    Microsoft.EntityFramework.Design

--- HorrorMovieAPI/Startup.cs
+++ HorrorMovieAPI/Startup.cs
@@ -16,6 +16,10 @@ namespace HorrorMovieAPI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // Blir ett MVC Projekt
+            // Iochmed detta kan den använda sig av Controllers
+            services.AddMvc(options => options.EnableEndpointRouting = false)
+                .SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_3_0);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,15 +30,7 @@ namespace HorrorMovieAPI
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            app.UseMvc();
         }
     }
 }
