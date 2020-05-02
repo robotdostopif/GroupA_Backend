bc76dbba2e4ecd8bd3780dad062c21c2ebd12ee5
Andreas Brochs <55091646+AndreasBrochs@users.noreply.github.com>
2020-05-02 14:27:27 +0200

    create model actor and actorDTO

---
+++ HorrorMovieAPI/Models/ActorDTO.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace HorrorMovieAPI.Models
+{
+    public class ActorDTO : IEntity
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DOB { get; set; }
+        public string BirthTown { get; set; }
+        public string BirthCountry { get; set; }
+        public ICollection<Casting> Castings { get; set; }
+        public string RollName { get; set; }
+    }
+}
