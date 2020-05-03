95970436e0f4993da978b90bca48d2b157e98294
Anders Djerf <37343667+istorp@users.noreply.github.com>
2020-05-03 20:06:25 +0200

    istorp

---
+++ HorrorMovieAPI/Models/Genre.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace HorrorMovieAPI.Models
+{
+    public class Genre : IEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Movie> Movies { get; set; }
+         
+    }
+}
