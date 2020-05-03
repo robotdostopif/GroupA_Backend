0075ff41ba360ee7919070797afa14f0fedb8a9b
AndreMorad <56093049+AndreMorad@users.noreply.github.com>
2020-05-03 19:39:32 +0200

    @andremorad
    added Director and DirectorDTO in Models follder

---
+++ HorrorMovieAPI/Models/DirectorDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HorrorMovieAPI.Models
+{
+    public class DirectorDTO
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DOB { get; set; }
+        public string BirthThown { get; set; }
+        public string BirthCountry { get; set; }
+        public int MovieId { get; set; }
+    }
+}
