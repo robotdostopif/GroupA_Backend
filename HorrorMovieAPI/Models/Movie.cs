cdd0999e31c21ed4e1c27b7a8b5fc4f8a73249d2
AndreMorad <56093049+AndreMorad@users.noreply.github.com>
2020-05-06 14:19:47 +0200

    @andremorad
    Completed SKRAC-80

--- HorrorMovieAPI/Models/Movie.cs
+++ HorrorMovieAPI/Models/Movie.cs
@@ -9,17 +9,11 @@ namespace HorrorMovieAPI.Models
     public class Movie : IEntity
     {
         public int Id { get; set; }
-        [ForeignKey("GenreId")]
         public Genre Genre { get; set; }
-        [StringLength(100, MinimumLength = 1)]
         public string Title { get; set; }
-        [Range(0, 360)]
         public int Length { get; set; }
-        [Range(0, 3000)]
         public int Year { get; set; }
-        [ForeignKey("DirectorId")]
         public Director Director { get; set; }
         public ICollection<Casting> Castings { get; set; }
     }
 }
-
