f0ec4fb1fbbc04cc3cfa9e88d86a6fdc4ec73567
Pierre <pierre.nygard@gmail.com>
2020-05-06 14:17:09 +0200

    Completed SKRAC-73

--- HorrorMovieAPI/Program.cs
+++ HorrorMovieAPI/Program.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using HorrorMovieAPI.Data;
 using HorrorMovieAPI.Models;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -16,25 +15,7 @@ namespace HorrorMovieAPI
     {
         public static void Main(string[] args)
         {
-            var host = CreateHostBuilder(args).Build();
-
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                try
-                {
-                    var context = services.GetRequiredService<HorrorContext>();
-                    var initializer = new DbInitializer();
-                    initializer.Initialize(context).Wait();
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred while seeding the database.");
-                }
-            }
-
-            host.Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
