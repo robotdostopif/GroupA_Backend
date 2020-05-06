025c3b7d09f6d82bae3d8fef75403844ccaa833f
AndreMorad <56093049+AndreMorad@users.noreply.github.com>
2020-05-06 21:43:12 +0200

    Created DirectorRepository
    Completed SKRAC-62

---
+++ HorrorMovieAPI/Services/DirectorRepository.cs
@@ -0,0 +1,44 @@
+﻿using HorrorMovieAPI.DB_Context;
+using HorrorMovieAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HorrorMovieAPI.Services
+{
+    public class DirectorRepository : IRepository<Director>, IDirectorRepository
+    {
+        private readonly HorrorContext _context;
+
+        public DirectorRepository(HorrorContext context)
+        {
+            _context = context;
+        }
+
+        public Task<Director> Add(Director entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Director> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Director> Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Director>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Director> Update(Director entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
