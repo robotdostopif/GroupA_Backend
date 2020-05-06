db2cc51e00eb60f8934a0d66ef259e93579b7583
Anders <jj@jj.com>
2020-05-06 17:25:55 +0200

    SKRAC-67 added GenresController

---
+++ HorrorMovieAPI/Controllers/GenresController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace HorrorMovieAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GenresController : ControllerBase
+    {
+        
+    }
+}
