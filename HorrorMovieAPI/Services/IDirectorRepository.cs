39253d684bd848cf6feee58aff8e0d5601f5239c
AndreMorad <56093049+AndreMorad@users.noreply.github.com>
2020-05-06 20:16:07 +0200

    Create IDirectorRepository
    Completed SKRAC-76

---
+++ HorrorMovieAPI/Services/IDirectorRepository.cs
@@ -0,0 +1,17 @@
+﻿using HorrorMovieAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HorrorMovieAPI.Services
+{
+    public interface IDirectorRepository
+    {
+        Task<List<Director>> GetDirectorsByBirthTown(string birthTown);
+
+        Task<List<Director>> GetDirectorsByBirthCountry(string birthCountry);
+
+        Task<List<Director>> GetDirectorsAndIncludeMovies(bool includeMovies);
+    }
+}
