0d0dbed75ca690b21fa682ebcd36ca1439e6f2de
AndreMorad <56093049+AndreMorad@users.noreply.github.com>
2020-05-07 11:24:17 +0200

    Created DirectorController
    Added the conroller and
    dependency inversion for DirectorController

---
+++ HorrorMovieAPI/Controllers/DirectorController.cs
@@ -0,0 +1,21 @@
+﻿using HorrorMovieAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HorrorMovieAPI.Controllers
+{
+    [ApiController]
+    [Route("api/v1.0/[controller]")]
+    public class DirectorController : ControllerBase
+    {
+        private readonly DirectorRepository _repository;
+
+        public DirectorController(DirectorRepository repository)
+        {
+            _repository = repository;
+        }
+    }
+}
