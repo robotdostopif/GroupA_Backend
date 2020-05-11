9c6dcecf0d65e8a96a5b9df742fb38a4a4ba3387
istorp <sentoc@outlook.com>
2020-05-11 14:49:40 +0200

    SKRAC-86 add save remove get all

--- HorrorMovieAPI/Services/IRepository.cs
+++ HorrorMovieAPI/Services/IRepository.cs
@@ -6,7 +6,7 @@ namespace HorrorMovieAPI.Services
 {
     public interface IRepository<T> where T : class, IEntity
     {
-        Task<List<T>> GetAll();
+        Task<bool> Save();
         Task<T> Get(int id);
         Task<T> Add(T entity);
         Task<T> Update(T entity);
