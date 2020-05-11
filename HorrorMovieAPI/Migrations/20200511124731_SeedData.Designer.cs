da42ad8e8ba25649c173a06430c51a82b3ef7094
Pierre <pierre.nygard@gmail.com>
2020-05-11 14:49:34 +0200

    SKRAC-82 complete

--- HorrorMovieAPI/Migrations/20200511124731_SeedData.Designer.cs
+++ HorrorMovieAPI/Migrations/20200511124731_SeedData.Designer.cs
@@ -4,14 +4,16 @@ using HorrorMovieAPI.DB_Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HorrorMovieAPI.Migrations
 {
     [DbContext(typeof(HorrorContext))]
-    partial class HorrorContextModelSnapshot : ModelSnapshot
+    [Migration("20200511124731_SeedData")]
+    partial class SeedData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
