d4681dc25b3872b70b75f2e6588a21300245eb6c
AndreMorad <56093049+AndreMorad@users.noreply.github.com>
2020-05-11 15:00:40 +0200

    Created IControllerCRUD
    Completed SKRAC-93

---
+++ HorrorMovieAPI/Controllers/IControllerCRUD.cs
@@ -0,0 +1,17 @@
+﻿using HorrorMovieAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace HorrorMovieAPI.Controllers
+{
+    public interface IControllerCRUD<T> where T : class, IEntity
+    {
+        Task<ActionResult<T>> Get(int id);
+
+        Task<ActionResult<T>> Delete(int id);
+
+        Task<ActionResult> Post(T entity);
+
+        Task<ActionResult> Put(int id, T entity);
+    }
+}
