3bd203579d6c8e27c7a8211e8d09cf8bbaa2f15c
Pierre <pierre.nygard@gmail.com>
2020-05-11 21:04:58 +0200

    MovieRepo, IMovieRepo updated

--- HorrorMovieAPI/Services/IMovieRepository.cs
+++ HorrorMovieAPI/Services/IMovieRepository.cs
@@ -6,11 +6,13 @@ namespace HorrorMovieAPI.Services
 {
     public interface IMovieRepository
     {
-        Task<List<Movie>> TaskGetMoviesByYear(int year);
-        Task<List<Movie>> GetMoviesByMinLength(int length);
-        Task<List<Movie>> GetMoviesByMaxLength(int length);
-        Task<List<Movie>> GetMoviesByGenre(int genreId);
-        Task<Movie> GetMovieByIdWithActors(int id, bool includeActors);
-        Task<Movie> GetMovieByIdWithDirector(int id, bool includeDirector);
+        Task<List<Movie>> GetAll(
+            int year,
+            int maxLength,
+            int minLength,
+            bool includeActors,
+            bool includeDirector,
+            int genreId
+            );
     }
 }
