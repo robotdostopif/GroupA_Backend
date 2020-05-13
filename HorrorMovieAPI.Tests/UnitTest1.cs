e4f16db40549513c427fe856262ec0bfa3a0009d
Pierre <pierre.nygard@gmail.com>
2020-05-13 11:09:21 +0200

    SKRAC-84 Started

--- HorrorMovieAPI.Tests/UnitTest1.cs
+++ HorrorMovieAPI.Tests/UnitTest1.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using HorrorMovieAPI.Models;
 using Xunit;
 
 namespace HorrorMovieAPI.Tests
@@ -8,7 +10,23 @@ namespace HorrorMovieAPI.Tests
         [Fact]
         public void Test1()
         {
+            Assert.Equal(1, 1);
+        }
 
+        private static IList<Director> GetDirectors()
+        {
+            return new List<Director>
+            {
+                new Director
+                {
+                    Id = 1,
+                    FirstName = "John",
+                    LastName = "Carpenter",
+                    DOB = DateTime.Parse("1948-01-16"),
+                    BirthTown = "Carthage, New York",
+                    BirthCountry = "USA"
+                }
+            };
         }
     }
 }
