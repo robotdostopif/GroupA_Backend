aaf27bf0527917a8dbd2d528e3f907487218f3c0
istorp <sentoc@outlook.com>
2020-05-20 11:36:22 +0200

    SKRAC-114 Create MoviesController Method GetById(int id)

--- HorrorMovieAPI/Controllers/MoviesController.cs
+++ HorrorMovieAPI/Controllers/MoviesController.cs
@@ -27,5 +27,13 @@ namespace HorrorMovieAPI.Controllers
             var mappedResults = _mapper.Map<MovieDTO[]>(result);
             return Ok (mappedResults);
         }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<MovieDTO>> GetById(int id, bool includeActors = false, bool includeDirector = false)
+        {
+            var result = await _repository.GetById(id, includeActors, includeDirector);
+            var mappedResult = _mapper.Map<MovieDTO>(result);
+            return Ok(mappedResult);
+        }
     }
 }
