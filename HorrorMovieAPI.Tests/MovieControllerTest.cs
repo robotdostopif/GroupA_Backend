9dff9494dddceb90055d11a2937d96d7598a1bc2
simbasang <sebastian.englund@live.se>
2020-05-28 10:04:17 +0200

    no workint test yet

--- HorrorMovieAPI.Tests/MovieControllerTest.cs
+++ HorrorMovieAPI.Tests/MovieControllerTest.cs
@@ -1,34 +1,89 @@
-﻿using HorrorMovieAPI.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
 using HorrorMovieAPI.Controllers;
+using HorrorMovieAPI.DB_Context;
+using HorrorMovieAPI.Dto;
+using HorrorMovieAPI.Models;
 using HorrorMovieAPI.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Moq;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
-namespace CoolestMovieAPI.Tests
+namespace HorrorMovieAPI.Tests.ControllerTests
 {
-    public class MovieControllerTest
+    public class MovieControllerTests
     {
-        [Fact]
-        public async Task GetAll_ReturnsOK()
+        private readonly Mock<HorrorContext> _mockContext;
+        private readonly Mock<IMovieRepository> _mockRepo;
+        private readonly Mock<IUrlHelper> _urlHelper;
+        private readonly Mock<IMapper> _mockMapper;
+        private readonly MoviesController _movieController;
+        public MovieControllerTests()
         {
-            // Arange
-          
-            
-            var directorRepositoryMock = new Mock<IMovieRepository>();
-            //directorRepositoryMock.Setup(repo => repo.GetAll(false, false)).ReturnsAsync((IList<Movie>)null);
-            var controller = new MovieController(directorRepositoryMock.Object);
+            _mockContext = new Mock<HorrorContext>();
+            _mockRepo = new Mock<IMovieRepository>();
+            _mockMapper = new Mock<IMapper>();
+            _urlHelper = new Mock<IUrlHelper>();
+            _movieController = new MoviesController(_mockRepo.Object, _mockMapper.Object, _urlHelper.Object);
+        }
+
+        [Fact]
+        public async void CreateDirector_DirectorExists_ReturnsCorrectDirectorId()
+        {
+            // Arrange
+            DirectorForUpdateDTO director = new DirectorForUpdateDTO()
+            {
+                DOB = DateTime.Parse("1944-01-20")
+            };
+            var result = await _directorController.CreateDirector(director);
+
+            Assert.IsType<CreatedResult>(result);
+        }
+
+        [Fact]
+        public async Task GetDirectorById_DirectorExists_ReturnsObjectResult()
+        {
+            Director director = new Director()
+            {
+                Id = 1,
+                FirstName = "Hasse",
+                DOB = DateTime.Parse("1944-01-20")
+            };
+            _mockRepo.Setup(repo => repo.GetDirectorById(1, true)).ReturnsAsync(director);
 
             // Act
-            var result = await controller.GetAll();
+            var response = await _directorController.GetDirectorById(1, true);
 
             // Assert
-            Assert.IsType<OkObjectResult>(result.Result);
+            Assert.IsAssignableFrom<ObjectResult>(response.Result);
+        }
+
+        [Fact]
+        public async Task GetDirectors_DirectorExists_ReturnsObjectResult()
+        {
+            // Arrange
+            List<Director> directors = new List<Director>() {
+                new Director() {
+                    Id = 1,
+                    FirstName = "Hasse",
+                    DOB = DateTime.Parse("1944-01-20")
+                },
+                new Director() {
+                    Id = 2,
+                    FirstName = "Tage",
+                    DOB = DateTime.Parse("1944-01-20")
+                }
+            };
+            _mockRepo.Setup(repo => repo.GetAll("", true)).ReturnsAsync(directors);
+
+            // Act
+            var response = await _directorController.GetAll("", true);
+
+            // Assert
+            Assert.IsAssignableFrom<ObjectResult>(response.Result);
         }
     }
-
 }
