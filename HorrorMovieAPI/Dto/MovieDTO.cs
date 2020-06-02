2c3f23d82620a25a64c62ae9302e0aef47890340
Nor Shiervani <nor.shier97@gmail.com>
2020-06-02 09:18:56 +0200

    XML documentaton for MovieDTO and MovieForUpdateDTO.

--- HorrorMovieAPI/Dto/MovieDTO.cs
+++ HorrorMovieAPI/Dto/MovieDTO.cs
@@ -6,14 +6,38 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HorrorMovieAPI.Models
 {
+    /// <summary>
+    /// Represents the data transfer object of a Movie which will be used during GET-requests.
+    /// </summary>
     public class MovieDTO : IEntity
     {
+        /// <summary>
+        /// Primary key of the movie.
+        /// </summary>
         public int Id { get; set; }
+        /// <summary>
+        /// Genre of the movie.
+        /// </summary>
         public GenreDTO Genre { get; set; }
+        /// <summary>
+        /// Title of the movie.
+        /// </summary>
         public string Title { get; set; }
+        /// <summary>
+        /// The length of the movie in minutes.
+        /// </summary>
         public int Length { get; set; }
+        /// <summary>
+        /// The year which the movie was released.
+        /// </summary>
         public int Year { get; set; }
+        /// <summary>
+        /// The person who directed this movie.
+        /// </summary>
         public DirectorDTO Director { get; set; }
+        /// <summary>
+        /// The list of people who acted in the movie.
+        /// </summary>
         public ICollection<CastingDTO> Castings { get; set; }
     }
 }
